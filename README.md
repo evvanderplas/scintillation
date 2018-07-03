e0dface079dd0e6d99ecaf8db75743f7d3444cb8
evvanderplas <evvanderplas@gmail.com>
2018-07-03 09:17:07 +0200

    Better plots

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # scintillation
+
+Software meant to read output of the Septentrio scintillation measurement instrument
+and analyze its data to monitor space weather. GNSS satellites transmit radio waves
+and the amplitude fluctuations and phase shift that are measured on the ground are a
+measure for the change of the refractive index of the medium.
+
+## Code
+The code is in python, and uses numpy, pandas and matplotlib to analyze the data.
--- test_ismr.py
+++ test_ismr.py
@@ -77,49 +77,132 @@ def plot_df(df, id, var):
     plotdata = df[df.SVID == id][var].values
     xdata = df[df.SVID == id]['t']
 
-    print('shape of plotdata: {}'.format(plotdata.shape))
 
     fig, ax = plt.subplots()
     ax.plot(xdata, plotdata)
     # ax.plot(plotdata)
-    fig.savefig('testplot_{}.png'.format(var))
+    out = 'testplot_{}_sat{}.png'.format(var,str(id).zfill(2))
+    print('shape of plotdata: {}'.format(plotdata.shape))
+    fig.savefig(out)
     plt.close(fig)
 
-def plot_az_el(df, id, var):
+def plot_df_multivar(df, id, varlist, name='multi'):
+    '''
+        Plot a list of variables from the dataframe
+    '''
+
+    iddf = df[df.SVID == id]
+    # print('shape of df for sat {}: {}, head: {}, {}'.format(id, iddf.shape, iddf.head(), varlist))
+    xdata = iddf.t
+    ydata = iddf[varlist].astype('float') # necessary
+
+
+    fig, ax = plt.subplots()
+    ydata.plot(x=xdata, ax=ax)
+
+    out = 'testplot_{}_sat{}.png'.format(name, str(id).zfill(2))
+    fig.savefig(out)
+    plt.close(fig)
+
+def azel_to_xy(df, id=None):
+    '''
+        Compute x,y from azimuth and elevation in dataframe
+    '''
+
+    if id is not None:
+        iddf = df[df.SVID == id]
+    else:
+        iddf = df
+
+    azdata = 2. * np.pi * (iddf['azimuth']/360.)
+    eldata = 2. * np.pi * (iddf['elevation']/360.)
+
+    x = np.cos(eldata) * np.sin(azdata)
+    y = np.cos(eldata) * np.cos(azdata)
+
+    return x,y
+
+def plot_az_el(df, id, var, cmap='jet'):
     '''
         Plot a variable as function of azimuth and elevation angle
+        following:
+        x = cos e * cos phi
+        y = sin e * sin phi
+
     '''
 
     plotdata = df[df.SVID == id][var].values
-    azdata = np.sin(2. * np.pi * (df[df.SVID == id]['azimuth']/360.))
-    eldata = np.cos(2. * np.pi * (df[df.SVID == id]['elevation']/360.))
+    x, y = azel_to_xy(df, id)
 
-    print('AZ/EL: shape of plotdata: {}'.format(plotdata.shape))
+    print('AZ/EL: shape of plotdata: ({}, {}): {}'.format(len(x), len(y), plotdata.shape))
 
     fig, ax = plt.subplots()
-    azel = ax.scatter(azdata, eldata, c=plotdata) #, c=plotdata)
+    azel = ax.scatter(y, x, c=plotdata, cmap=plt.cm.get_cmap(cmap))
     # ax.plot(plotdata)
-    ax.set_xlabel('sin [ Azimuth]')
-    ax.set_ylabel('cos [ Elevation ]')
+    ax.set_xlabel('x = cos [ Elevation] sin [ Azimuth ]')
+    ax.set_ylabel('y = cos [ Elevation ] cos [ Azimuth ]')
     plt.colorbar(azel, ax=ax)
-    fig.savefig('azelplot_{}.png'.format(var))
+    fig.savefig('azelplot_{}_sat{}.png'.format(var, str(id).zfill(2)))
     plt.close(fig)
 
+def plot_az_el_multisat(df, var, cmap='jet'):
+    '''
+        Plot a variable as function of azimuth and elevation angle
+        following:
+        x = cos e * cos phi
+        y = sin e * sin phi
+
+    '''
+
+    fig, ax = plt.subplots()
+
+    satellites = df['SVID'].unique()
+    for sat in satellites:
+        if sat < 37:
+            iddf = df[df.SVID == sat]
+            plotdata = iddf[var].values
+            x, y = azel_to_xy(iddf) # (df, id)
+            print('For sat {} we have {} - {} - {}'.format(sat, len(x), len(y), len(plotdata)))
+            azel = ax.scatter(x, y, c=plotdata, cmap=plt.cm.get_cmap(cmap), label='sat {}'.format(sat))
+
+    ax.set_xlabel('x = cos [ Elevation] sin [ Azimuth ]')
+    ax.set_ylabel('y = cos [ Elevation ] cos [ Azimuth ]')
+    # plt.colorbar(azel, ax=ax)
+    fig.savefig('azelplot_{}_multisat.png'.format(var))
+    plt.close(fig)
 
 def main():
     '''
         Main script
     '''
 
-    weeksecondstoutc(1811,164196.732,16) ## --> '2014-09-22 21:36:52'
+    # weeksecondstoutc(1811,164196.732,16) ## --> '2014-09-22 21:36:52'
 
+    teclist = ['sig1_TEC_m45', 'sig1_TEC_m30', 'sig1_TEC_m15', 'sig1_TEC']
+    dteclist = ['sig1_dTEC_m60_m45', 'sig1_dTEC_m45_m30', 'sig1_dTEC_m30_m15', 'sig1_dTEC_m15_0']
+    philist = ['sig1_phi01', 'sig1_phi03', 'sig1_phi10', 'sig1_phi30', 'sig1_phi60']
+    sig4_list = ['sig1_S4', 'sig2_S4', 'sig3_S4']
 
     ismrfile = 'KNMI283M.17_.ismr'
     print('ISMR file; {}, available: {}'.format(ismrfile, os.path.isfile(ismrfile)))
 
     data = read_ismr(ismrfile)
-    plot_df(data, 1, 'sig1_S4')
+
     plot_az_el(data, 1, 'sig1_S4')
+    plot_az_el_multisat(data, 'sig1_S4', cmap='RdBu_r')
+    plot_az_el_multisat(data, 'sig1_TEC', cmap='hot')
+
+    satellites = data['SVID'].unique()
+    for sat in satellites:
+        if sat < 37:
+            plot_df_multivar(data, sat, teclist, name='multitec')
+            plot_df_multivar(data, sat, dteclist, name='deltatec')
+            plot_df_multivar(data, sat, philist, name='mphase')
+            plot_df_multivar(data, sat, sig4_list, name='sigN')
+
+
+    plot_df(data, 1, 'sig1_S4')
+    plot_df(data, 1, 'sig1_TEC')
 
 if __name__ == '__main__':
     main()
